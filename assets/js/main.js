ea5b1a02b6896bda5b17d2dc4aab434c09c5b68e
rukanomic <lucas_brito8@hotmail.com>
2022-12-06 01:06:50 -0300

    JS atualizado com inicio de ingração com PokeAPI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 
-console.log('Sucesso!')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+const y = 10 + 10
+console.log(y)
